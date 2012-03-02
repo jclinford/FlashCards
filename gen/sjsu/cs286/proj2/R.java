2df28ec2af9270ca46b187377402fc5d403a6e7e
jc linford <jclinford@gmail.com>
2012-03-01 17:58:45 -0800

    updated ui

--- gen/sjsu/cs286/proj2/R.java
+++ gen/sjsu/cs286/proj2/R.java
@@ -13,8 +13,28 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int addButton=0x7f05000e;
+        public static final int button1=0x7f05000a;
+        public static final int button2=0x7f050009;
+        public static final int buttonLayout=0x7f05000d;
+        public static final int definitionText=0x7f050007;
+        public static final int definitionView=0x7f050006;
+        public static final int linearLayout1=0x7f050000;
+        public static final int linearLayout2=0x7f050002;
+        public static final int linearLayout3=0x7f050005;
+        public static final int linearLayout4=0x7f050008;
+        public static final int listView=0x7f05000f;
+        public static final int nameText=0x7f050004;
+        public static final int nameView=0x7f050003;
+        public static final int textView1=0x7f050001;
+        public static final int title=0x7f05000c;
+        public static final int titleLayout=0x7f05000b;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int add_word=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int main_landscape=0x7f030002;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
